99f9bb99126475a15510f6631a793ceead6e4c8a
ElenaVerdy <sahal9894@gmail.com>
2020-06-14 15:47:44 +0300

    .

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(function(req, res, next) { 
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Origins", process.env.PORT ? 'https://parchees-82bf1.web.app/' : 'http://192.168.1.67:3000/');
+    console.log(process.env.PORT ? 'https://parchees-82bf1.web.app/' : 'http://192.168.1.67:3000/');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
