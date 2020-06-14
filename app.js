865ebc7178b29f26dfc1e4a11deb2a33427cd221
ElenaVerdy <sahal9894@gmail.com>
2020-06-14 14:57:00 +0300

    .

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const server = app.listen(process.env.PORT || 8080, () => console.log(`Listening
 const io = require("socket.io")(server);
 
 app.use(express.static(__dirname + '/public'));
-io.set('origins', process.env.PORT ? 'https://vk.com/' : 'http://192.168.1.67:3000/');
+io.set('origins', process.env.PORT ? 'https://parchees-82bf1.web.app/' : 'http://192.168.1.67:3000/');
 
 const tables = [];
 tables.remove = function(tableId) {
