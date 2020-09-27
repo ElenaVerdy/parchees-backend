951ee255d22be15defc23c4c0cff583c40ef0938
ElenaVerdy <sahal9894@gmail.com>
2020-09-27 16:22:17 +0300

    .

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ const bodyParser    = require('body-parser');
 let topByRank       = [];
 let topByChips      = [];
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
 app.use(function(req, res, next) { 
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Origins", process.env.PORT ? 'https://parchees-82bf1.web.app/' : 'http://192.168.1.67:3000/');
